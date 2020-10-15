dc30fc9a22d7fd80ba60e471827ba49bae10019d
Brennen <bhogan1@nd.edu>
2020-10-15 17:59:09 -0400

    Update mask_no_mask.py

--- mask_no_mask.py
+++ mask_no_mask.py
@@ -3,27 +3,38 @@ import os
 import random
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import torchvision
 from torchvision import torch,datasets,transforms,models
 from torch.utils.data import Dataset,DataLoader
+import torch
+from torch import nn
 from PIL import Image
 import numpy as np 
+import ssl
+
+#Certificate for downloading the pretrained model
+ssl._create_default_https_context = ssl._create_unverified_context
+
+#Boolean which controls if the images are displayed or not
+SHOW_IMAGES = False
 
 #Colors cooresponding to the labels
 class_color={"face_no_mask":"r","face_with_mask":"g"}
+label_to_ints={"face_no_mask":0,"face_with_mask":1}
+ints_to_label={0:"face_no_mask",1:"face_with_mask"}
 
 # creating the function to visualize images and draw a box around the face
 def visualize_training(image_name):
     #Reads in the image and creates the plot
-    img=plt.imread(os.path.join('./mask_data/dataset/masks/images',image_name))
+    image=plt.imread(os.path.join('./mask_data/dataset/masks/images',image_name))
     temp=train[train.name==image_name]
     fig,ax=plt.subplots(1)
     ax.axis('off')
     fig.set_size_inches(20,10)
-    ax.imshow(img)
+    ax.imshow(image)
     for i in range(len(temp)):
         #Grab the x and y bounds of the face from the training data
         x1,x2,y1,y2=temp.values[i][1:5]
-        print(temp.values[i])
         #Draw the rectange around the face
         patch=patches.Rectangle((x1,x2),y1-x1,y2-x2,linewidth=2, 
                                 edgecolor=class_color[temp.values[i][5]],facecolor="none",)
@@ -32,18 +43,18 @@ def visualize_training(image_name):
         ax.add_patch(patch)
 
     #Plots the image with the label
-    imgplot = plt.imshow(img)
+    imgplot = plt.imshow(image)
     plt.show()
 
 # creating the function to visualize images and draw a box around the face
 def visualize_guess(image_name, labels):
     #Reads in the image and creates the plot
-    img=plt.imread(os.path.join('./mask_data/dataset/masks/images',image_name))
+    image=plt.imread(os.path.join('./mask_data/dataset/masks/images',image_name))
     temp=train[train.name==image_name]
     fig,ax=plt.subplots(1)
     ax.axis('off')
     fig.set_size_inches(20,10)
-    ax.imshow(img)
+    ax.imshow(image)
     for i in range(len(temp)):
 
         #Grab the x and y bounds of the face from the training data
@@ -56,27 +67,51 @@ def visualize_guess(image_name, labels):
         ax.add_patch(patch)
 
     #Plots the image with the label
-    imgplot = plt.imshow(img)
+    imgplot = plt.imshow(image)
     plt.show()
 
-class MaskAndNoMask(Dataset): 
+def train_model(model, training_data):
+    epochs=3
+    training_loss=[]
+
+    for epoch in range(epochs):
+        train_loss=0
+        
+        #Work with the training data in batches of 10 to make program more responsive
+        for batch,(data,target) in enumerate(training_data):
+
+            optimizer.zero_grad()
+            output=model(data)
+            loss=criterion(output,target)
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item()
+        
+            if batch%10==9:
+                print("Epoch: {}, Total Images: {}\nAverage Training Loss: {}".format(epoch+1,batch+1,train_loss/10))
+                training_loss.append(train_loss) 
+                train_loss=0.0
+    
+    print(training_loss)
+
+#Class used to crop the images and standardize them to the same aspect ratio
+class ImageStandardizer(Dataset): 
     def __init__(self,dataframe,root_dir,transform=None):
         self.annotation=dataframe
         self.root_dir=root_dir
-        self.transform=transform
+        self.transform=transform      
         
     def __len__(self):
         return len(self.annotation)
     
     def __getitem__(self,index):
-        print(self.annotation.iloc[index,0])
-        print("T: {}".format(self.annotation.iloc[index,1:4]))
-        img_path=os.path.join(self.root_dir,self.annotation.iloc[index,0])
-        new_img=Image.open(img_path).crop((self.annotation.iloc[index,1:5]))
-        label=torch.tensor(int(self.annotation.iloc[index,5]))
+        image_path=os.path.join(self.root_dir,self.annotation.iloc[index,0])
+        cropped_image=Image.open(image_path).crop((self.annotation.iloc[index,1:5]))
+        label=torch.tensor(label_to_ints[self.annotation.iloc[index,5]])
     
+        #Resizes the cropped image to 224 x 224
         if self.transform:
-            image=self.transform(new_img)
+            image=self.transform(cropped_image)
             return(image,label)
 
 if __name__ == '__main__':
@@ -89,31 +124,55 @@ if __name__ == '__main__':
     print(train.classname.value_counts())
     print("-----")
 
-    visualize_training(random.choice(train.name.values))
-    visualize_guess(random.choice(train.name.values), ['face_no_mask', 'face_with_mask', 'face_no_mask', 'face_with_mask', 'face_no_mask', 'face_with_mask'])
+    if SHOW_IMAGES:
+        visualize_training(random.choice(train.name.values))
+        visualize_guess(random.choice(train.name.values), ['face_no_mask', 'face_with_mask', 'face_no_mask', 'face_with_mask', 'face_no_mask', 'face_with_mask'])
 
     #Training data makes up 75% of the dataset and testing data makes up the remaining 25%
     train_size=int(len(train)*0.75)
     test_size=len(train)-train_size
-    my_transform=transforms.Compose([transforms.Resize((224,224)),
+    image_transformer=transforms.Compose([transforms.Resize((224,224)),
                                  transforms.RandomCrop((224,224)),
                                  transforms.ToTensor()])
 
-    path_images=os.path.join("./mask_data/dataset/masks/images")
+    image_path=os.path.join("./mask_data/dataset/masks/images")
 
-    dataset=MaskAndNoMask(dataframe=train,root_dir=path_images,transform=my_transform)
+    dataset = ImageStandardizer(train, image_path, image_transformer)
 
     batch_size=32
 
-    trainset,testset=torch.utils.data.random_split(dataset,[train_size,test_size])
-    train_loader=DataLoader(dataset=trainset,batch_size=batch_size,shuffle=True)
-    test_loader=DataLoader(dataset=testset,batch_size=batch_size,shuffle=True)
+    trainset,testset = torch.utils.data.random_split(dataset,[train_size,test_size])
+    training_data    = DataLoader(trainset, batch_size, True)
+    testing_data     = DataLoader(testset, batch_size, True)
 
-    dataiter=iter(train_loader)
+    dataiter=iter(training_data)
     images,labels=dataiter.next()
-    images=images.numpy()
 
-    fig=plt.figure(figsize=(25,4))
-    for idx in np.arange(20):
-        ax=fig.add_subplot(2,20/2,idx+1,xticks=[],yticks=[])
-        plt.imshow(np.transpose(images[idx],(1,2,0)))
+    if SHOW_IMAGES:
+        for i in np.arange(20):
+            fig,ax=plt.subplots(1)
+            ax.axis('off')
+            plt.imshow(np.transpose(images[i],(1,2,0)))
+            plt.show()
+
+    print(images.size())
+    print(labels.size())
+
+    #Download the pre-trained rsnet facial recognition model
+    model = torchvision.models.resnet34(True)
+
+    input_layer  = model.fc.in_features
+    output_layer = nn.Linear(input_layer,2)
+    model.fc.out_features = output_layer
+
+    print(model.fc.out_features)
+
+    #Sets the loss function
+    criterion=nn.CrossEntropyLoss()
+
+    #Sets the model learning rate
+    optimizer=torch.optim.SGD(model.parameters(),lr=0.005)
+
+    train_model(model, training_data)
+
+    print(done)

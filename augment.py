b4b9e0c6624b8f119c69cbc454611c1ea828d672
Brennen <bhogan1@nd.edu>
2020-10-24 13:28:42 -0400

    new datasets

--- augment.py
+++ augment.py
@@ -6,6 +6,7 @@ df = pd.read_csv('train.csv')
 #Grabs the data with the specified categories and writes to training file
 mask_df = df.loc[(df['classname'] == "face_with_mask")]
 no_mask_df = df.loc[(df['classname'] == "face_no_mask")]
+#face_with_mask_incorrect = df.loc[(df['classname'] == "face_with_mask_incorrect")]
 pd.concat([
     mask_df,
-    no_mask_df]).to_csv('train_simple.csv', index=False)
+    no_mask_df]).to_csv('mask_no_mask.csv', index=False)
